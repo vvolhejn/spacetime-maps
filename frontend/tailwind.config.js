1a015a3941d7194411a457eebfbb54a883f5a420
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-11-06 20:19:48 +0100

    Customize font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*{.js,.jsx,.ts,.tsx,.html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#252a34"
+      }
+    },
+    fontFamily: {
+      sans: ["Gabarito, sans-serif", {
+        fontVariationSettings: "'wght' 400",
+      }],
+    },
   },
   plugins: [],
 }
