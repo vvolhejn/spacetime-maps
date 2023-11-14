f2e7b58a4025f1339f15bb715ade7cc62e206e84
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-11-14 22:52:00 +0100

    Switch to Vite for building

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*{.js,.jsx,.ts,.tsx,.html}"],
+  content: ["./src/**/*{.js,.jsx,.ts,.tsx,.html}", "./index.html"],
   theme: {
     extend: {
       colors: {
