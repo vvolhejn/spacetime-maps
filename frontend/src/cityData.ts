6a0386fe759951f2784b809177de2b2835c04297
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-11-20 22:19:48 +0100

    Get rid of useless "dir" attribute

--- frontend/src/cityData.ts
+++ frontend/src/cityData.ts
@@ -3,7 +3,6 @@ import { GridData } from "./gridData";
 export type CityMetadata = {
   displayName: string;
   maxTimeness: number;
-  dir: string;
 };
 
 export type City = CityMetadata & {
@@ -15,22 +14,18 @@ export const CITIES: { [key: string]: CityMetadata } = {
   prague: {
     displayName: "Prague",
     maxTimeness: 0.05,
-    dir: "prague",
   },
   zurich: {
     displayName: "Zürich",
     maxTimeness: 0.05,
-    dir: "zurich",
   },
   london: {
     displayName: "London",
     maxTimeness: 0.15,
-    dir: "london",
   },
   newyork: {
     displayName: "New York",
     maxTimeness: 0.15,
-    dir: "newyork",
   },
 };
 
@@ -39,13 +34,12 @@ export type CityName = keyof typeof CITIES;
 export const DEFAULT_CITY = "zurich";
 
 export const fetchCity = async (cityName: CityName) => {
-  const cityMetadata = CITIES[cityName];
   return Promise.all([
-    import(`./assets/${cityMetadata.dir}/grid_data.json`),
-    import(`./assets/${cityMetadata.dir}/map.png`),
+    import(`./assets/${cityName}/grid_data.json`),
+    import(`./assets/${cityName}/map.png`),
   ]).then(([gridData, mapImage]) => {
     return {
-      ...cityMetadata,
+      ...CITIES[cityName],
       data: gridData.default as GridData,
       mapImage: mapImage.default as string,
     };
